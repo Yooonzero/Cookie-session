1c5cfc2dcf6a7450201bb3922250752d5d5f5d84
yooonzero <playlist808@kakao.com>
2023-06-20 19:16:09 +0900

    세션

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const express = require('express');
+const cookieParser = require('cookie-parser');
 const app = express();
+
+app.use(cookieParser());
 
 app.get('/set-cookie', (req, res) => {
     let expires = new Date();
@@ -9,6 +12,31 @@ app.get('/set-cookie', (req, res) => {
         expires: expires,
     });
     return res.status(200).end();
+});
+
+app.get('/get-cookie', (req, res) => {
+    // const cookie = req.headers.cookie;
+    const cookies = req.cookies; // cookieparser 미들웨어를 적용했기 때문에 사용할 수 있다.
+    console.log(cookies); // name=sparta
+    return res.status(200).json({ cookies });
+});
+
+// 사용자의 정보를 저장할 만한 자물쇠 (데이터를 저장하는 부분)
+let session = {}; // key-value() 이 형태로 열쇠 정보가 들어왔을 때 , 실제로 사용자의 정보를 사용할 수 있는 부분은 value 부분이다.
+app.get('/set-session', (req, res) => {
+    const name = 'sparta'; // 세션에 저장할 데이터
+    const uniqueInt = Date.now(); // 클라이언트에게 할당할 키값 (열쇠)값
+    session[uniqueInt] = name; // 세션에 데이터 저장을 완료했다
+    res.cookie('sessionKey', uniqueInt); // 클라이언트에게 키값을 할당함
+    res.status(200).end();
+});
+
+app.get('/get-session', (req, res) => {
+    const { sessionKey } = req.cookies;
+    const sessionItem = session[sessionKey];
+
+    console.log(sessionItem);
+    return res.status(200).json({ sessionItem: sessionItem });
 });
 
 app.listen(5002, () => {
